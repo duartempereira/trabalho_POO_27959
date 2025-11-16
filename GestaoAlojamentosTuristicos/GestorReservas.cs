86f2d7c88ebf7b358bd57e0ab17f37b0c9a0d29f
Duarte "macrogod" Pereira <duartepereira@pm.me>
2025-11-16 16:49:47 +0000

    Add GestorReservas class for reservation management
    Introduces the GestorReservas class to manage reservations, including methods to add and retrieve reservations and check accommodation availability. Some validation logic is marked as TODO for future implementation.

---
+++ GestaoAlojamentosTuristicos/GestorReservas.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoAlojamentosTuristicos
+{
+    internal class GestorReservas
+    {
+        #region Attributes
+        private List<Reserva> reservas;
+        #endregion
+
+        #region Constructors
+        public GestorReservas()
+        {
+            reservas = new List<Reserva>();
+        }
+        #endregion
+
+        #region Properties
+        public List<Reserva> Reservas { get; set; }
+        #endregion
+
+        #region Methods
+        public void AdicionarReserva(Reserva r)
+        {
+            // TODO: validar disponibilidade do alojamento na Fase 2
+            reservas.Add(r);
+        }
+
+        public List<Reserva> ObterTodasReservas()
+        {
+            return reservas;
+        }
+
+        public bool AlojamentoDisponivel(Alojamento aloj, DateTime checkIn, DateTime checkOut)
+        {
+            // TODO: implementar validação de datas na Fase 2
+            return true;
+        }
+        #endregion
+    }
+}
